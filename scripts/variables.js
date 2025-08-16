0511d6862cbe5fad01c8cc382149969846cd1797
Israel Dias <israeldiasoliveira97@gmail.com>
2025-08-16 08:55:51 -0300

    Incluindo JS e comentarios no JS

---
+++ scripts/variables.js
@@ -0,0 +1,15 @@
+// Var aceita redeclaracao e reatribuicao
+var nome = "Israel";
+nome = "arthur";
+console.log(nome);
+
+// Let nao aceita redeclaracao e aceita reatribuicao
+let idade = 29;
+idade = 27;
+console.log(idade);
+
+//Const nao aceita redeclaracao nem reatribuicao
+const profissao = "desenvolvedor";
+profissao = 'professor'
+console.log(profissao);
+
